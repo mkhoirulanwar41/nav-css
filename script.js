0f889727e50ca5026d7e2d0fbd43b32b6821a119
mkhoirulanwar41 <mkhoirulanwar41.rumahgrafis41@gmail.com>
2025-09-21 14:14:31 +0700

    fixing problem

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const menuToggle = document.querySelector('.menu-toggle');
 const navUl = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', () => {
-    navUl.style.display = navUl.style.display === 'flex' ? 'none' : 'flex';
+    // navUl.style.display = navUl.style.display === 'flex' ? 'none' : 'flex';
+    // if(navUl.style.display === 'flex' || navUl.style.display === 'none'){
+    //     navUl.style.color = 'white';
+    // }
+    // navUl.style.display = 'flex';
+    if(navUl.style.display === 'flex'){
+        navUl.style.display = 'none';
+    }else {
+        navUl.style.display = 'flex';
+    }
 });
+
